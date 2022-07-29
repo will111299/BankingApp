7d233f6bff85776c7e56fef249dae723a8325492
ITUStudent <ITUStudent@ML-RefVm-594359>
2022-07-29 11:02:06 -0400

    Clock Holiday Tests

--- Banking.Tests.Unit/StandardBusinessClockTests.cs
+++ Banking.Tests.Unit/StandardBusinessClockTests.cs
@@ -28,9 +28,55 @@ public class StandardBusinessClockTests
 
     // We are closed on the following days:
     // Weekends (Saturday and Sunday)
-    // 7/4 - Fourth of July
-    // 12/25 - Christmas
-    // 4/20 - Jeff's Birthday
+    [Theory]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(30)]
+    [InlineData(31)]
+    public void ClosedOnWeekends(int dayOfJuly2022)
+    {
+        var stubbedClock = new Mock<ISystemTime>();
+        IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
+        stubbedClock.Setup(c => c.GetCurrent()).Returns(new DateTime(2022, 7, dayOfJuly2022, 12, 00, 00));
 
-    
+
+        Assert.False(clock.IsDuringBusinessHour());
+    }
+
+    // 7/4 - Fourth of July
+    [Fact]
+    public void ClosedOnFourthOfJuly()
+    {
+        var stubbedClock = new Mock<ISystemTime>();
+        IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
+        stubbedClock.Setup(c => c.GetCurrent()).Returns(new DateTime(2022, 7, 4, 12, 00, 00));
+
+
+        Assert.False(clock.IsDuringBusinessHour());
+    }
+
+    // 12/25 - Christmas
+    [Fact]
+    public void ClosedOnChristmas()
+    {
+        var stubbedClock = new Mock<ISystemTime>();
+        IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
+        stubbedClock.Setup(c => c.GetCurrent()).Returns(new DateTime(2022, 12, 25, 12, 00, 00));
+
+
+        Assert.False(clock.IsDuringBusinessHour());
+    }
+
+    // 4/20 - Jeff's Birthday
+    [Fact]
+    public void ClosedOnJeffsBirthday()
+    {
+        var stubbedClock = new Mock<ISystemTime>();
+        IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
+        stubbedClock.Setup(c => c.GetCurrent()).Returns(new DateTime(2022, 4, 20, 12, 00, 00));
+
+
+        Assert.False(clock.IsDuringBusinessHour());
+    }
+
 }
